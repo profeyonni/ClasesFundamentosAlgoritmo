57fa8c58158a8cdfbcde0bf3d9a93764e6360677
Yonni <profeyonni@gmail.com>
2025-03-29 22:37:19 -0500

    Primer Commit p2

---
+++ PrimerProyectoVS/ProyectoAdicionalVS/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoAdicionalVS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Proyecto Adicional");
+        }
+    }
+}
